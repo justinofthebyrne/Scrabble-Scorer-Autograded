fd998ff07253182329e32c52dc8c01a78af828d0
justinofthebyrne <jpburnside3@gmail.com>
2024-02-05 19:48:53 -0600

    Up to Part 1

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -1,6 +1,8 @@
 // This assignment is inspired by a problem on Exercism (https://exercism.org/tracks/javascript/exercises/etl) that demonstrates Extract-Transform-Load using Scrabble's scoring system. 
 
 const input = require("readline-sync");
+console.log("Let's play some scrabble!");
+let letterPoints = input.question("Enter a word:")
 
 const oldPointStructure = {
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
@@ -28,15 +30,32 @@ function oldScrabbleScorer(word) {
 	}
 	return letterPoints;
  }
-
+ console.log(oldScrabbleScorer(letterPoints));
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   
 };
 
 let simpleScorer;
+
+// function simpleScorer(word) {
+//    word = word.toLowerCase === word.toLowerCase;
+//    let pointValue = "";
+
+//    for (let i = 0; i < word.length; i++) {
+
+//       for (const pointValue in simpleScorer) {
+//          if (simpleScorer[pointValue].includes(word[i])) {
+//             pointValue += `Points for '${word[i]}': ${pointValue}\n`
+//          }
+        
+//       }
+//    }
+//    return pointValue; 
+// }
+// console.log(simpleScorer(pointValue));
 
 let vowelBonusScorer;
 
